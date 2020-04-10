067ef39a2f81d49cd56b5ca406c45dbced387ae4
Cody Hoover <codydevloop@gmail.com>
2020-04-09 17:17:42 -0700

    parsed oneCall and reformated time

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,12 @@
 // API Key b52ce1773e76080cb950272fcf749391
-// documentation 
+// documentation
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+let apiKey = "b52ce1773e76080cb950272fcf749391"
 
 $(document).ready(function () {
 
-    var movieNameC = $(this).attr("data-name");
-    var queryURL = "https://www.omdbapi.com/?t=" + movieNameC + "&apikey=80df7118";
+    // let movieNameC = $(this).attr("data-name");
+    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=phoenix&units=imperial&APPID="+apiKey;
 
 
         $.ajax({
@@ -13,14 +15,44 @@ $(document).ready(function () {
         })
 
         .then(function (response){
-            // let m = $("movie-view");
-            // console.log(response);
-            // movieView = $("#move-view");
-            // console.log(movieView);
-            // console.log(m);
 
-            // $("#movie-view").text(JSON.stringify(response));
-            $("#movie-view").text(JSON.stringify(response));
+            // console.log(response);
+
+            // Get the lat and long
+            // console.log(response.coord.lon);
+            let longitude = response.coord.lon;
+            let latitiude = response.coord.lat;
+            console.log(response);
+
+            let cityName = response.name;
+            // console.log("this is city name :"+cityName);
+
+            //make call to One Call API
+            // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={YOUR API KEY}
+
+            $.ajax({
+                url:"http://api.openweathermap.org/data/2.5/onecall?lat="+latitiude+"&lon="+longitude+"&units=imperial&appid="+apiKey,
+                method: "GET"
+            })
+
+            .then(function(oneCall){
+                
+                console.log(oneCall);
+                // let searchedData = JSON.stringify(oneCall);
+                // console.log("this is One CAll: " + searchedData);
+
+                let timeFromOneCall = oneCall.current.dt;
+                console.log("This is city timie : "+timeFromOneCall);
+
+                let newFormatTime = moment.unix(timeFromOneCall).format('dddd, MMMM Do, YYYY');
+
+                // let timeUTCconversion = moment.utc(timeFromOneCall);
+                console.log(newFormatTime);
+                // let regularTime = moment(timeUTCconversion, 'MM/DD/YYYY');
+                
+
+
+            });
 
         });
 
